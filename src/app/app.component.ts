bf5053d7103d1bbd405e2cdc71e516d2ea15e221
Falynsky <kamil.falinski@gmail.com>
2019-11-07 15:18:08 +0100

    AMIW - 7.11.2019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {logger} from 'codelyzer/util/logger';
+import {log} from 'util';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,35 @@ export class AppComponent {
   title = 'amiwCalculator';
 
   clickMessage = '';
+  public val = 3;
+  public result = '';
+  private val1: number;
+  private val2: number;
 
-  clickMe() {
-    this.clickMessage = 'TEST';
-    logger.error();
+  addOne() {
+    this.result = this.result + '1';
+  }
+
+  addZero() {
+    this.result = this.result + '0';
+  }
+
+  clearAll() {
+    this.result = '';
+    this.val1 = 0;
+    this.val2 = 1;
+  }
+
+  noNumber(e: KeyboardEvent) {
+    console.log(e);
+    if (e.key !== '0' && e.key !== '1') {
+      e.preventDefault();
+    }
+    console.log(e.keyCode);
+  }
+
+  operationResult() {
+    this.result = '';
   }
 }
 
